e579f244236861a1e921f74086a2f62cfd3859f5
erick <ericksonoliva@gmail.com>
2023-03-27 17:16:44 -0600

    se agrego modelo productos, se pueden crear y ver

---
+++ db/migrate/20230327225232_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :title, null: false
+      t.text :description
+      t.integer :price, null: false
+
+      t.timestamps
+    end
+  end
+end
