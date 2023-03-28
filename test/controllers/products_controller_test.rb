3473105bb8a7ac3ab5f7621572198d2805d661e5
erick <ericksonoliva@gmail.com>
2023-03-28 10:02:53 -0600

    se agrego test para creacion y logo redigire a home

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -5,4 +5,37 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     get products_index_url
     assert_response :success
   end
+
+  test 'renders a new product form' do
+    get new_product_path
+
+    assert_response :success
+    assert_select 'form'
+  end
+
+  test 'allow to create a new product' do
+    post products_path, params: {
+      product: {
+        title: 'Sticker Zelda',
+        description: 'tamano 4 x 5',
+        price: 25
+      }
+    }
+
+    assert_redirected_to new_product_path
+    assert_equal flash[:notice], "Producto creado correctamente"
+  end
+
+  test 'does not allow to create a new product with empty fields' do
+    post products_path, params: {
+      product: {
+        title: '',
+        description: 'tamano 4 x 5',
+        price: 25
+      }
+    }
+
+    assert_response :unprocessable_entity
+  end
+
 end
