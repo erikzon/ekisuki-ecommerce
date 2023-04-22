78b6f18476cac9cbeb073fce78ac1a7a6191d8c1
erick <ericksonoliva@gmail.com>
2023-04-21 22:04:30 -0600

    se agrego la libreria de manera local para prescindir del cdn

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "tailwindcss-stimulus-components", to: "https://ga.jspm.io/npm:tailwindcss-stimulus-components@3.0.4/dist/tailwindcss-stimulus-components.modern.js"
+pin "tailwindcss-stimulus-components" # @3.0.4
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.1
