ed0ef40793a7adbed874da7f9e36789ea6abd378
erick <ericksonoliva@gmail.com>
2023-05-15 13:16:40 -0600

    menu mobile javascript corregido

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,6 +7,8 @@ application.debug = false
 window.Stimulus   = application
 
 import { Alert } from "tailwindcss-stimulus-components"
+import Menu_controller from "./controllers/menu_controller";
 application.register('alert', Alert)
+application.register('menu', Menu_controller)
 
 export { application }
--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -4,6 +4,6 @@ export default class extends Controller {
     static targets = ["items"]
 
     toggle() {
-        this.itemsTarget.classList.toggle('translate-x-full')
+        setTimeout(this.itemsTarget.classList.toggle('-translate-x-full'),300)
     }
 }
