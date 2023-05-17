c2ca764f68784566d523aa1601b78157d2e0d3ac
erick <ericksonoliva@gmail.com>
2023-05-16 20:12:16 -0600

    agregar https obligatoriamente a todas las peticiones

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module EkisukiEcommerce
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.hosts << ".ekisuki.com"
+    config.force_ssl = true
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
