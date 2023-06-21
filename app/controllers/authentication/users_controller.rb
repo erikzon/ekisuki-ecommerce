54a26365af36cc5b082222fe277dd5169d5468d3
erick <ericksonoliva@gmail.com>
2023-06-21 17:42:18 -0600

    correccion bug recaptcha

--- app/controllers/authentication/users_controller.rb
+++ app/controllers/authentication/users_controller.rb
@@ -9,10 +9,10 @@ class Authentication::UsersController < ApplicationController
   def create
     @category = Category.all
     @user = User.new(user_params)
-    if verify_recaptcha(model: @user) && @user.save
+    if verify_recaptcha(model: @user, message: "Error al validar reCaptcha, intenta nuevamente") && @user.save
       UserMailer.with(user: @user).welcome.deliver_later
       session[:user_id] = @user.id
-      redirect_to products_path, notice: "Usuario creado, ya puedes empezar a comprar!"
+      redirect_to categories_path, notice: "Usuario creado, ya puedes empezar a comprar!"
     else
       render :new, status: :unprocessable_entity, alert: "Ocurrio un error, intenta de nuevo mas tarde."
     end
@@ -21,6 +21,6 @@ class Authentication::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :username, :password, :password_confirmation)
+    params.require(:user).permit(:email, :username, :password, :password_confirmation,:authenticity_token)
   end
 end
